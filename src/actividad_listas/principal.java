9cb7a4bfd3bb6cb3c9143875f8863d3f8ae19cb1
lykan3 <josemiguel.cardenasmorales@unitecnar.edu.co>
2025-04-18 15:19:48 -0500

    carga inicial

---
+++ src/actividad_listas/principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad_listas;
+import FORMULARIO.fmrusuario;
+/**
+ *
+ * @author LYKAN
+ */
+public class principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        fmrusuario n = fmrusuario ();
+        n.setVisible(true);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
